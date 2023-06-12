0d499c0b54317a142946e552b9af0b00724d67fb
Daniel Sparber <daniel@sparber.io>
2023-06-12 09:34:36 +0200

    sonar_cloud.yml: gdal:ubuntu-small-latest

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import logging
 
-from elevation import ElevationCentralSwitzerland
+from elevation import ElevationSwitzerland
 
 if __name__ == '__main__':
     logging.root.setLevel(logging.INFO)
 
-    layer = ElevationCentralSwitzerland()
+    layer = ElevationSwitzerland()
     layer.download_tiles()
     layer.create_virtual_dataset()
