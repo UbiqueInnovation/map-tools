0d2906c45212552f6611c0a94657e8ef35f85336
Daniel Sparber <daniel@sparber.io>
2023-11-10 17:41:58 +0100

    Added public read

--- commons/bucket_tile_output.py
+++ commons/bucket_tile_output.py
@@ -10,4 +10,8 @@ class BucketTileOutput(TileOutput):
         self.base_path = base_path
 
     def upload(self, file_path: str, tile_info: TileInfo) -> None:
-        self.bucket.upload_file(file_path, f"{self.base_path}/{tile_info.path}.png")
+        self.bucket.upload_file(
+            file_path,
+            f"{self.base_path}/{tile_info.path}.png",
+            ExtraArgs={"ACL": "public-read"},
+        )
