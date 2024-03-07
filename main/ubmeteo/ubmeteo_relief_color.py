d85a67e3e68b1f9b031495cbff3f0ae59195d59f
Daniel Sparber <sparber@ubique.ch>
2024-03-07 13:54:00 +0100

    Adjusted output paths for ubmeteo color

--- main/ubmeteo/ubmeteo_relief_color.py
+++ main/ubmeteo/ubmeteo_relief_color.py
@@ -2,7 +2,7 @@ import logging
 from datetime import timedelta
 
 from commons import CompositeTileOutput, BucketTileOutput, R2Client
-from datasets import Dataset
+from datasets import Glo90
 from elevation import ElevationTools
 from tiles import WebmercatorTileInfo
 
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     tiles = list(WebmercatorTileInfo(zoom=0, x=0, y=0).descendants(max_zoom=10))
 
     for style in ["light"]:
-        dataset = Dataset("Glo90/1k.tif")
-        storage_path = f"v1/map/relief-color/{style}"
+        storage_path = f"v1/map/3857/relief-color/{style}"
+        dataset = Glo90()
         ElevationTools.generate_color_relief_tiles(
             tile_infos=tiles,
             dataset=dataset,
@@ -28,12 +28,12 @@ if __name__ == "__main__":
             output=CompositeTileOutput(
                 [
                     BucketTileOutput(
-                        bucket=r2.maps_dev,
+                        bucket=r2.ubmeteo_app_dev,
                         base_path=storage_path,
                         cache_control=cache_control_test,
                     ),
                     BucketTileOutput(
-                        bucket=r2.maps_prod,
+                        bucket=r2.ubmeteo_app_prod,
                         base_path=storage_path,
                         cache_control=cache_control_prod,
                     ),
