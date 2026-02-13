9485ae2ad4b0bd6dc2a67e7cb01204155d71ecdf
Daniel Sparber <sparber@ubique.ch>
2026-02-13 12:03:24 +0100

    try to fix github action

--- datasets/tests/test_swiss_alti_3d.py
+++ datasets/tests/test_swiss_alti_3d.py
@@ -6,8 +6,8 @@ from datasets import SwissAlti3d
 class TestSwissAlti3d(TestCase):
     def test_get_low_res_urls(self) -> None:
         layer = SwissAlti3d()
-        self.assertEqual(len(list(layer.get_urls())), 43_628)
+        self.assertEqual(len(list(layer.get_urls())), 43_650)
 
     def test_get_high_res_urls(self) -> None:
         layer = SwissAlti3d(high_res=True)
-        self.assertEqual(len(list(layer.get_urls())), 43_628)
+        self.assertEqual(len(list(layer.get_urls())), 43_650)
